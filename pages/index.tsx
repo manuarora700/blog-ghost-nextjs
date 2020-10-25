825b5f9683b97e46ea5508014393556da5204749
Manu Arora <mannuarora700@gmail.com>
2020-10-25 17:42:52 +0530

    slug done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,24 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
+import Link from 'next/link'
 
 const CONTENT_API_KEY = 'cf1950cbb71a9436ec40d9fe80';
 const BLOG_URL = 'https://ghost-next-backend.herokuapp.com';
 
 type Post = {
+  title: string
+  slug: string
+
 
 }
 
 async function getPosts() {
-  const res = await fetch(`${BLOG_URL}/ghost/api/v3/content/posts/?key=${CONTENT_API_KEY}`)
+  const res = await fetch(`${BLOG_URL}/ghost/api/v3/content/posts/?key=${CONTENT_API_KEY}&fields=title,slug,excerpt,custom_excerpt`)
               .then(res => res.json());
 
-  const titles = res.posts.map((post) => post.title);
-  // console.log(titles);
-  return titles;
+  const posts = res.posts;
+  // console.log(res);
+  return posts;
 }
 
 export const getStaticProps = async ({ params }) => {
@@ -25,14 +29,20 @@ export const getStaticProps = async ({ params }) => {
   }
 }
 
-const Home:React.FC<{ posts: string[] }> = (props) =>{
+const Home:React.FC<{ posts: Post[] }> = (props) =>{
   const { posts } = props
   return (
     <div className={styles.container}>
       <h1>Hello to my blog! :)</h1>
       <ul>
         {posts.map((post, index) => {
-          return <li key={index}>{post}</li>
+          return <li key={post.slug}>
+            
+              <Link href="/post/[slug]" as={`/post/${post.slug}`}>
+                <a>{post.title}</a>
+              </Link>
+
+            </li>
         })}
       </ul>
     </div>
